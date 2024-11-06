2243c99b7d8fd1106ddf5174cb876530a42b6239
radoslawazukowska <radoslawa.zuk@gmail.com>
2024-11-06 12:33:32 +0000

    added api handling different indexes

--- API/src/main/java/org/ulpgc/api/controller/SearchEngineController.java
+++ API/src/main/java/org/ulpgc/api/controller/SearchEngineController.java
@@ -1,6 +1,7 @@
 package org.ulpgc.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.ulpgc.query_engine.ResponseList;
@@ -19,6 +20,11 @@ public class SearchEngineController {
     @GetMapping("/search")
     public ResponseList getSearchResults(@RequestParam String word) {
         return searchEngine.searchForBooksWithWord(word);
+    }
+
+    @GetMapping("/search/{indexer}")
+    public ResponseList getSearchResults(@PathVariable String indexer, @RequestParam String word) {
+        return searchEngine.searchForBooksWithWord(word, indexer);
     }
 
     @GetMapping("/text")
