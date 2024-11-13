a21ee56754be10ee6bbac02fd5ae94bbc443a49f
radoslawazukowska <radoslawa.zuk@gmail.com>
2024-11-13 18:16:41 +0000

    added interface for search engine controller

--- API/src/main/java/org/ulpgc/api/controller/SearchEngineControllerInterface.java
+++ API/src/main/java/org/ulpgc/api/controller/SearchEngineControllerInterface.java
@@ -9,15 +9,7 @@ import org.ulpgc.query_engine.SearchEngine;
 import org.ulpgc.query_engine.TextFragment;
 
 @RestController
-public class SearchEngineController implements SearchEngineControllerInterface {
-
-    private final SearchEngine searchEngine;
-
-    public SearchEngineController() {
-        this.searchEngine = new SearchEngine();
-    }
-
-    @Override
+public interface SearchEngineControllerInterface {
     @GetMapping("/search/{indexer}")
     public MultipleWordsResponseList getSearchResultsMultiple(
             @PathVariable String indexer,
@@ -26,16 +18,10 @@ public class SearchEngineController implements SearchEngineControllerInterface {
             @RequestParam(required = false) String author,
             @RequestParam(required = false) String date,
             @RequestParam(required = false) String language
-    ) {
-        String[] words = word.split(" ");
-        return searchEngine.searchForMultiplewithCriteria(indexer, words, title, author, date, language);
-    }
+    );
 
-    @Override
     @GetMapping("/text")
     public TextFragment getTextFragment(
             @RequestParam Integer textId,
-            @RequestParam Integer wordPos) {
-        return searchEngine.getPartOfBookWithWord(textId, wordPos);
-    }
+            @RequestParam Integer wordPos);
 }
