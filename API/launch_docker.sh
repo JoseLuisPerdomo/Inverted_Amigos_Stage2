6b77fdbc04cd703ecbab0428765c17afd115cdf6
radoslawazukowska <radoslawa.zuk@gmail.com>
2024-12-03 14:38:34 +0000

    added creating docker and running it for api

---
+++ API/launch_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#mvn clean install
+#docker build -t api:1.0
+
+cd ..
+
+CURRENT_DIR=$(pwd)
+
+cd API
+
+DATAMART_PATH="$CURRENT_DIR/datamart2"
+TRIE_INDEX_PATH="$CURRENT_DIR/trie_index_by_prefix"
+INVERTED_INDEX_PATH="$CURRENT_DIR/InvertedIndex/datamart"
+BOOKS_PATH="$CURRENT_DIR/gutenberg_books"
+METADATA_PATH="$CURRENT_DIR/gutenberg_data.txt"
+
+docker stop api-container
+docker rm api-container
+
+docker run -d \
+  -p 8080:8080 \
+  -v "$DATAMART_PATH:/app/datamart2" \
+  -v "$TRIE_INDEX_PATH:/app/trie_index_by_prefix" \
+  -v "$INVERTED_INDEX_PATH:/app/InvertedIndex/datamart" \
+  -v "$BOOKS_PATH:/app/gutenberg_books" \
+  -v "$METADATA_PATH:/app/gutenberg_data.txt" \
+  --name api-container api:1.0
